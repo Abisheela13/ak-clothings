fa9ff3b4e8cdd8055141256b7521c9a94d211155
seeman <seeman79202@gmail.com>
2026-06-08 16:24:43 +0530

    Fix duplicate exports in vite.config.js (merge config)

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/ak-clothings/',
   plugins: [react()],
-})
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
 })
